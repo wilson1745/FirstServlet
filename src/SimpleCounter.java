a36455814e553f28c8f6774b9585e8d844cf3ae6
Lo Wei-Shen <38279941+wilson1745@users.noreply.github.com>
2018-11-04 10:18:51 +0800

    again

--- src/SimpleCounter.java
+++ src/SimpleCounter.java
@@ -7,25 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class SimpleCounter
- */
 public class SimpleCounter extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	int counter = 0;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public SimpleCounter() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter pw = response.getWriter();
